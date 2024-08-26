be1d6746bdb6a240772d32d4a103fbf98a933418
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-08-26 12:57:35 +0530

    Day -1 notes-TypeScript & Angular

---
+++ Day-1/first.js
@@ -0,0 +1,10 @@
+var a;
+a = 56;
+var b = 12;
+var fname = "NIKUNJ SONI";
+function test(num1, num2) {
+    return num1 + num2;
+}
+console.log(a + b);
+console.log(fname);
+console.log("Addition:" + test(10, 34));
---
+++ Day-1/first.ts
@@ -0,0 +1,15 @@
+let a:number;
+a=56;
+
+let b:number=12;
+
+let fname:string="NIKUNJ SONI";
+
+function test(num1:number,num2:number){
+    return num1+num2;
+}
+
+
+console.log(a+b);
+console.log(fname);
+console.log("Addition:"+test(10,34));

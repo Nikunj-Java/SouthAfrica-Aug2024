f85ad9f97b94933a5b3b088c8dd35daf8274d6eb
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-08-26 15:24:02 +0530

    Day -1 notes-TypeScript & Angular

--- Day-1/myapp/src/app/binding/binding.component.ts
+++ Day-1/myapp/src/app/binding/binding.component.ts
@@ -13,6 +13,13 @@ export class BindingComponent implements OnInit {
   }
   name:string="NIKUNJ SONI";
   url:string="https://www.google.com/";
+
+  products=[{name:"pencile",price:"8.3",availability:"26-08-2024",rating:4.8},
+    {name:"pen",price:"10",availability:"27-08-2024",rating:4.5},
+    {name:"eraser",price:"8.9",availability:"26-08-2024",rating:5.0},
+    {name:"scale",price:"12",availability:"26-08-2024",rating:4.9},
+    {name:"books",price:"15",availability:"30-08-2024",rating:5.0}
+  ]
   
 
 }

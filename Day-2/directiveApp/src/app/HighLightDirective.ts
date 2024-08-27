ad7f986e09a6cd54d228e93cfefc26ef1134f7ca
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-08-27 14:43:26 +0530

    ngModel-binding,Structural Directive,Custom Directives,LAB-1

--- Day-2/directiveApp/src/app/HighLightDirective.ts
+++ Day-2/directiveApp/src/app/HighLightDirective.ts
@@ -4,6 +4,6 @@ import { Directive, ElementRef, Renderer2 } from "@angular/core";
 @Directive({selector:'[highlight]'})
 export class HighLightDirective{
     constructor(er:ElementRef,render:Renderer2){
-        render.setStyle(er.nativeElement,'background-color','aqua');
+        render.setStyle(er.nativeElement,'background-color','yellow');
     }
 }

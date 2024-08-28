982b433b8f7034e9e58bfa98b29f585109e859ec
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-08-28 13:27:45 +0530

    Pipes In Angular,Routing

--- Day-3/pipesApp/src/app/app.module.ts
+++ Day-3/pipesApp/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PipesComponent } from './pipes/pipes.component';
 import { CustomPipes } from './CustomPipe';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { GallaryComponent } from './gallary/gallary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PipesComponent,
-    CustomPipes
+    CustomPipes,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent,
+    GallaryComponent
   ],
   imports: [
     BrowserModule,
